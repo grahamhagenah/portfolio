803970bb3953e3b7a55ce954d17a922e64d89bc9
Graham Hagenah <gwhagenah@gmail.com>
2023-12-06 00:22:50 -0500

    updating home page animation

--- src/bundle.js
+++ src/bundle.js
@@ -1,22 +1,36 @@
+let mosaicWrapper
+let thirdItem
+let thirdPosition = -300
+let firstPosition = -250
+
 document.addEventListener('DOMContentLoaded', function() {
-  // Your function code here
-  handleScroll();
-  console.log("hello")
-});
 
+  mosaicWrapper = document.querySelector('.mosaic-wrapper')
+  firstItem = document.getElementsByClassName('mosaic-item')[0]
+  thirdItem = document.getElementsByClassName('mosaic-item')[2]
 
-// Function to handle scroll event
-function handleScroll() {
-  const mosaicWrapper = document.querySelector('.mosaic-wrapper');
-  
-  // Calculate the amount of scroll
-  const scrollOffset =  window.scrollY / -8;
-  
-  // Apply the transformation (translateX) based on scroll offset
-  mosaicWrapper.style.transform = `translateX(${scrollOffset}px)`;
-}
+  // Initialize starting location and scale
+  thirdItem.style.transform = `translateY(${thirdPosition}px)`
+  firstItem.style.transform = `translateY(${firstPosition}px)`
 
-// Add a scroll event listener to call the handleScroll function
-window.addEventListener('scroll', handleScroll);
+  // Scroll event listener to handle the home page animation
+  window.addEventListener("scroll", function() {
 
-console.log(scrollOffset)
+    // Calculate scroll, slow it down + reverse direction
+    let scrollOffset =  window.scrollY / -8 
+
+    mosaicWrapper.style.transform = `translateX(${scrollOffset}px)` 
+
+    let newThirdPosition = thirdPosition + window.scrollY
+    let newFirstPosition = firstPosition + window.scrollY
+
+    calculatePosition(firstPosition, firstItem)
+    calculatePosition(thirdPosition, thirdItem)
+  })
+
+  function calculatePosition(startingPosition, item) {
+    let newPostion = startingPosition + window.scrollY
+    return (newPostion >= 0) ? item.style.transform = "translateY(0)" : item.style.transform = `translateY(${newPostion}px)`
+  }
+
+})
